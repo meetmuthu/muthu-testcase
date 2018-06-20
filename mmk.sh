75e8c2e0480175753e4a32bf5c44f42523ff7feb
meetmuthu <meet.king.muthu@gmail.com>
2018-06-20 16:52:41 +0530

    first commit

---
+++ mmk.sh
@@ -0,0 +1,12 @@
+##Sample script for testing
+
+host=`hostname`
+greet="welcome to $host !"
+msg="Lets Get Started"
+clear
+echo "********************"
+echo "$greet"
+echo "$msg"
+echo "********************"
+echo ""
+echo ""
